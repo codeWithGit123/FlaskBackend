9654fd3b4bfc059584639992a70e751a9e43ae6b
codeWithGit123 <mdabdur2004@gmail.com>
2025-03-14 01:29:59 +0530

    h

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def pred():
     return send_file(result_image_path, mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000,debug=True)   
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)  
